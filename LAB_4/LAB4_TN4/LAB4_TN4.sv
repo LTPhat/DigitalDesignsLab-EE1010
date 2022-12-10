56f6d8bf2ee96b5e27b0711322a93c89e2dd15bf
LTPhat <102889533+LTPhat@users.noreply.github.com>
2022-12-10 09:18:48 +0700

    update

--- LAB_4/LAB4_TN4/LAB4_TN4.sv
+++ LAB_4/LAB4_TN4/LAB4_TN4.sv
@@ -1,74 +1,85 @@
-module LAB4_TN4(
-  input clk,
-  input rst,
-  output reg [6:0] seg_0,
-  output reg [6:0] seg_1); 
-  integer i = 0;
-  logic out;
-  logic[3:0] bcd;
-  logic clock;
-  always_ff@(posedge clk)
-	begin
-     i = i + 1;
-     if (i == 25000000) begin
-           clock = ~ clock;
-           i = 0;
-     end
+module bai4 (
+	input logic re,
+	input logic clk,
+	output logic [6:0] o1,o2
+);
+logic out;
+integer k = 0;
+always_ff@(posedge clk) begin
+	k = k+1;
+	if ( k == 25000000) begin
+		out = ~out;
+		k = 0;
 	end
-	
-  always@(posedge clock or posedge rst)
-  begin
-  if (rst == 1) 
-		begin
-			bcd <= 0;
-       end
-	else
-		begin
-			bcd <= bcd + 1'b1;
-		end
-	end
-	
-   always @(bcd) begin
-        case (bcd)
-					4'b0000: seg_0 = 7'b1000000; 
-					4'b0101: 
-						seg_0 = 7'b0010010; 
-					4'b0110: 
-						seg_0 = 7'b0000010; 
-					4'b0111: 
-					seg_0 = 7'b1111000; 
-					4'b1000: 
-					seg_0 = 7'b0000000; 
-					4'b1001: 
-					seg_0 = 7'b0010000; 
-					4'b1010: 
-					seg_0 = 7'b0001000; 
-					4'b1011: 
-					seg_0 = 7'b1111001; 
-					4'b1100: 
-					seg_0 = 7'b0100100; 
-					4'b1101: 
-					seg_0 = 7'b0011001; 
-					4'b1110: 
-					seg_0 = 7'b0000110; 
-					default :
-					seg_0 = 7'b1111111; 
-        endcase
-		  end
-always @(bcd) begin
-case(bcd)
-	4'b0000: seg_1 = 7'b1000000;
-	4'b0101:	seg_1 = 7'b1000000;
-	4'b0110: seg_1 = 7'b1000000;
-	4'b0111: seg_1 = 7'b1000000;
-	4'b1000:	seg_1 = 7'b1000000;
-	4'b1001: seg_1 = 7'b1000000;
-	4'b1010: seg_1 = 7'b1111001;
-	4'b1011: seg_1 = 7'b1111001;
-	4'b1100: seg_1 = 7'b1111001;
-	4'b1101: seg_1 = 7'b1111001;
-	4'b1110: seg_1 = 7'b1111001;
-	default seg_1 = 7'b1111111; 
-	endcase
 end
-endmodule
+logic [3:0] i;
+always @(posedge out or posedge re) begin
+if (re == '1)
+     i <= 4'b0000;
+else
+if (i == 4'b1110 || i == 4'b0000)
+     i <= 4'b0101;
+else
+     i <= i + 4'b0001;
+end
+seg sus (
+	.i(i),
+	.o1(o1),
+	.o2(o2)
+);
+endmodule: bai4
+
+module seg (
+	input  logic [3:0] i,
+	output logic [6:0] o1,o2
+);
+always_comb begin
+case(i)
+4'b0101: begin
+ o1 = 7'b1000000;
+ o2 = 7'b0010010;
+  end
+4'b0110: begin
+ o1 = 7'b1000000;
+ o2 = 7'b0000010;
+  end
+4'b0111: begin 
+ o1 = 7'b1000000;  
+ o2 = 7'b1111000; 
+  end
+4'b1000: begin 
+ o1 = 7'b1000000;  
+ o2 = 7'b0000000; 
+  end
+4'b1001: begin 
+ o1 = 7'b1000000;  
+ o2 = 7'b0010000; 
+  end
+4'b1010: begin 
+ o1 = 7'b1111001;  
+ o2 = 7'b1000000; 
+  end
+4'b1011: begin 
+ o1 = 7'b1111001;  
+ o2 = 7'b1111011; 
+  end
+4'b1100: begin 
+ o1 = 7'b1111001;  
+ o2 = 7'b0100100; 
+  end
+4'b1101: begin 
+ o1 = 7'b1111001;  
+ o2 = 7'b0110000; 
+  end
+4'b1110: begin 
+ o1 = 7'b1111001;  
+ o2 = 7'b1111000; 
+  end
+4'b0000: begin
+ o1 = 7'b1000000;
+ o2 = 7'b1000000;
+  end
+  default: begin o1 = 7'b1111111; o2 = 7'b1111111; end
+endcase
+end
+endmodule: seg
